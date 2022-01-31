537fef2ccf986fe2b8d2b94d6347b0ad1c48ff31
Jelena Janković <jjankovic@smart.edu.rs>
2022-01-31 11:05:08 +0100

    commit

---
+++ Cas01/rad.js
@@ -0,0 +1,13 @@
+555mlmlm
+VM41:1 Uncaught SyntaxError: Invalid or unexpected token
+int a=5;
+VM77:1 Uncaught SyntaxError: Unexpected identifier
+var a=5;
+undefined
+if(a>5);
+    
+    
+undefined
+if(a>5)
+{}
+undefined
---
+++ Cas02/zadatak02.js
@@ -0,0 +1,32 @@
+var niz=[1,2,3,4,5,6,7];
+
+for(i=0;i<niz.length;i++)
+    if(i%2)
+        console.log(niz[i]);
+VM314:5 2
+VM314:5 4
+VM314:5 6
+undefined
+for(i=0;i<niz.length;i++)
+        console.log(niz[i]);
+VM356:2 1
+VM356:2 2
+VM356:2 3
+VM356:2 4
+VM356:2 5
+VM356:2 6
+VM356:2 7
+undefined
+brojac=0
+0
+for(i=0;i<niz.length;i++)
+        if(niz[0]<niz[i])
+            brojac++
+5
+var suma = 0
+undefined
+suma
+0
+for(i=0;i<niz.length;i++)
+       suma+=niz[i];
+28
